2e8db24f78c73615a39d9debee7867be0485fdb4
Pradyumna Shembekar <pradyumna@hey.com>
2023-04-14 20:55:16 -0400

    Support ActiveSupport deprecator.

--- lib/devise.rb
+++ lib/devise.rb
@@ -522,11 +522,15 @@ module Devise
   end
 
   def self.activerecord51? # :nodoc:
-    ActiveSupport::Deprecation.warn <<-DEPRECATION.strip_heredoc
+    Devise.deprecator.warn <<-DEPRECATION.strip_heredoc
       [Devise] `Devise.activerecord51?` is deprecated and will be removed in the next major version.
       It is a non-public method that's no longer used internally, but that other libraries have been relying on.
     DEPRECATION
     defined?(ActiveRecord) && ActiveRecord.gem_version >= Gem::Version.new("5.1.x")
+  end
+
+  def self.deprecator
+    @deprecator ||= ActiveSupport::Deprecation.new("5.0", "Devise")
   end
 end
 
